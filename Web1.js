46ea431e55383721d3608cea60595114cd259e3c
mamin081359-spec <m.amin081359@gmail.com>
2026-04-16 09:35:00 +0300

    Add functions to change display of elements

---
+++ Web1.js
@@ -0,0 +1,28 @@
+
+function change(){
+   let dor = document.getElementById('door')
+   let win = document.getElementById('wind')
+   dor.style.display="flex"
+   setTimeout(function(){
+      win.style.display="none"}, 1000)
+}
+function chang(){
+   let dor = document.getElementById('door')
+   let break_fas = document.getElementById('break')
+   let win = document.getElementById('wind')
+   setTimeout(function(){
+      break_fas.style.display="none"
+      win.style.display="flex"
+      dor.style.display="none"}, 1000)
+}
+function chang_break(){
+   let dor = document.getElementById('door')
+   let win = document.getElementById('wind')
+   let break_fas = document.getElementById('break')
+   
+   setTimeout(function(){
+      break_fas.style.display="flex"
+      win.style.display="none"
+      dor.style.display="none"}, 1000)
+}
+
